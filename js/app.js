491a273201ba9b55e1dadf2d65442c6f03ba5067
Wilson Jan Sasam <wsasam.flatx@gmail.com>
2017-07-15 17:34:10 +0800

    added kml

--- js/app.js
+++ js/app.js
@@ -4,4 +4,9 @@ function initMap() {
         center: {lat: 10.340298, lng: 123.922477},
         zoom: 15
     });
+
+    var ctaLayer = new google.maps.KmlLayer({
+        url: 'https://flatx.github.io/js/layer.kml',
+        map: map
+    });
 }
